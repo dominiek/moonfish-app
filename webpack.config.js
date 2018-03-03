df48e6d951fb8bba6b6c68ff0f0060c6636f1100
Kaare Larsen <me@kaareal.com>
2018-03-03 22:38:03 +0100

    bugfix

--- webpack.config.js
+++ webpack.config.js
@@ -73,7 +73,7 @@ module.exports = {
     }, {
       test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
       loader: 'url-loader?limit=10000&minetype=application/font-woff'
-    }
+    }]
   },
   plugins
 };
