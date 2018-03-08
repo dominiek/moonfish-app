50b7f577972d19ef020c1d7475f7151199e4d63b
Peter Cuhalev <peter@yukaii.com>
2018-03-08 17:12:25 +0100

    Registration page styled + tweaks

--- src/components/PageCenter.js
+++ src/components/PageCenter.js
@@ -5,11 +5,11 @@ import { Grid } from 'semantic-ui-react';
 export default (props) => (
   <div style={{ height: '100%' }}>
     <Grid
-      style={{ height: '100%' }}
+      style={{ minHeight: '100%' }}
       centered
       verticalAlign="middle"
     >
-      <Grid.Column style={{ maxWidth: 450 }}>
+      <Grid.Column style={{ maxWidth: '500px', margin: '40px 0' }}>
         { props.children }
       </Grid.Column>
     </Grid>
